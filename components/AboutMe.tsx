fbffa3a3b17f229791d2b25b79b6ca06ad300515
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-21 21:00:02 +0300

    Update AboutMe.tsx
    Twitter x logosu

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faInstagram, faX-twitter, faDiscord, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faInstagram, faXTwitter, faDiscord, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import type { NextPage } from 'next';
 import type { PageProps } from '../types';
 
