bb0f195324ac133fe8e43e30fc56449f63f924e2
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-21 20:20:28 +0300

    Update Organizations.tsx
    Organizasyonlar düzenlendi!

--- components/Organizations.tsx
+++ components/Organizations.tsx
@@ -6,28 +6,16 @@ import type { PageProps } from '../types';
 const Organizations: NextPage = (pageProps: PageProps): JSX.Element => {
     const organizations = [
         {
-            title: 'Caneria Bot',
-            href: 'https://caneria.net/',
-            position: 'Founder',
-            icon: 'https://caneria.net/assets/icons/favicon-round.png'
-        },
-        {
-            title: 'Con City',
+            title: 'ConCity',
             href: 'https://con.city/',
-            position: 'Developer',
-            icon: 'https://cdn.discordapp.com/icons/1010858209881362432/acf3ffbee2e416cd505df2055b5045c3.png?size=256'
-        },
-        {
-            title: 'Alpaca Bot',
-            href: 'https://alpacabot.xyz/',
-            position: 'Lead Developer',
-            icon: 'https://alpacabot.xyz/assets/images/icons/cTkfInE.png'
+            position: 'Helper',
+            icon: 'https://media.discordapp.net/attachments/529376209977606144/1165338481753596004/icon_02b3af8233.png?ex=65467d08&is=65340808&hm=78acf0121c41242ab473d5050e0ed04636a039f3e40608414e6ed63ebf71077f&'
         }
     ];
 
     return (
         <div className='my-20 md:px-8 w-full flex flex-col items-center'>
-            <h2 className='font-bold text-lg uppercase'>Organizations</h2>
+            <h2 className='font-bold text-lg uppercase'>Organizasyonlar</h2>
             <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
                 {organizations.map((data, index) => <div className='duration-150 hover:shadow-[0_0px_30px_0px_#7947d6] m-4 bg-gray-500/10 px-4 py-2 rounded-lg' key={`techs${index}`}>
                     <a target={'_blank'} rel="noreferrer" href={data.href}
