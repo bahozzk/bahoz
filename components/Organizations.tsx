8a7f41f3e4f13332afbe496d58e4e77930cd6072
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-21 22:58:58 +0300

    Update Organizations.tsx

--- components/Organizations.tsx
+++ components/Organizations.tsx
@@ -17,7 +17,7 @@ const Organizations: NextPage = (pageProps: PageProps): JSX.Element => {
         <div className='my-20 md:px-8 w-full flex flex-col items-center'>
             <h2 className='font-bold text-lg uppercase'>Organizasyonlar</h2>
             <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
-                {organizations.map((data, index) => <div className='duration-150 hover:shadow-[0_0px_30px_0px_#fcb400] hover:shadow-[50%_0px_30px_0px_#ff4579] m-4 bg-gray-500/10 px-4 py-2 rounded-lg flex flex-row justify-between' key={`techs${index}`}>
+                {organizations.map((data, index) => <div className='duration-150 hover:shadow-[0_0px_30px_0px_#fcb400] hover:shadow-[0_50%_30px_0px_#ff4579] m-4 bg-gray-500/10 px-4 py-2 rounded-lg flex flex-row justify-between' key={`techs${index}`}>
                     <a target={'_blank'} rel="noreferrer" href={data.href}
                         className={`flex flex-row items-center justify-center py-1 w-[300px]`}>
                         <img className='mr-2' alt={`Organization ${index} - Icon`} width="120" height="120" src={data.icon} />
