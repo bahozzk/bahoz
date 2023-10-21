7130aa9369c3d32d3a85e51d6ce2a7d2b6bbd39c
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-21 19:25:30 +0300

    Update AboutMe.tsx
    Hakkımda kısmı güncellendi!

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -28,18 +28,18 @@ const AboutMe: NextPage = (pageProps: PageProps): JSX.Element => {
     ];
 
     const content = <p className='text-center my-4 text-lg text-white/80'>
-        Hi! I&apos;m a Jr Full Stack Web Developer and I&apos;m coding apps, bots, API&apos;s and websites.
+        Merhaba! Ben Bahoz, 14 yaşında lise öğrencisiyim.
         <br></br>
-        Also, since I&apos;m a high school student, I can only code in my spare time.
+        Yazılıma ilgiliyim fakat hiçbir dili bilmiyorum,
         <br></br>
-        But if you have a project in your mind, don&apos;t hesitate to contact me, I&apos;m a freelancer!
+        voleybol oynamayı çok seviyorum.
         <br></br>
-        Currently I am the owner and sole developer of the <a className='text-purple-400/90' target={'_blank'} href='https://caneria.net/' rel="noreferrer">Caneria</a>!
+        Şu anda <a className='text-orange-400/90' target={'_blank'} href='https://con.city/' rel="noreferrer">ConCity</a>'de Helper pozisyonunda çalışıyorum!
     </p>
 
     return (
         <div id="aboutme" className='my-4 md:px-8 w-full mb-20 flex flex-col items-center'>
-            <h2 className='font-bold text-lg uppercase'>About Me</h2>
+            <h2 className='font-bold text-lg uppercase'>Hakkımda</h2>
             <div className='flex flex-row flex-wrap items-center justify-center max-w-4xl'>
                 <div className='block'>
                     {content}
