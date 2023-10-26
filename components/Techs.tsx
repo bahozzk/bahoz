649a590e9e63031c1f7d174e5f65f1f0aa9e3675
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-26 17:10:40 +0300

    Update Techs.tsx

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faNodeJs, faSquareJs, faNpm, faReact, faVuejs, faGitAlt, faUbuntu } from '@fortawesome/free-brands-svg-icons';
+import ReactPlayer from 'react-player';
 import type { NextPage } from 'next';
 import type { PageProps } from '../types';
 
@@ -10,15 +11,29 @@ const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
       icon: <FontAwesomeIcon color={'#efd81d'} size="6x" icon={faSquareJs} />,
       href: 'https://www.javascript.com/'
     }
-  ]
+  ];
 
   return (
     <div className='my-20 md:px-8 w-full flex flex-col items-center'>
       <h2 className='font-bold text-lg uppercase'>Teknolojiler</h2>
       <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
-        {techs.map((data, index) => <a target={'_blank'} rel="noreferrer" href={data.href} className={`m-6 flex flex-row items-center`} key={`techs${index}`}>
-          {data.icon}
-        </a>)}
+        {techs.map((data, index) => (
+          <a
+            target={'_blank'}
+            rel="noreferrer"
+            href={data.href}
+            className={`m-6 flex flex-row items-center`}
+            key={`techs${index}`}
+          >
+            {data.icon}
+          </a>
+        ))}
+
+        <ReactPlayer
+          src="/assets/images/enerci.mp3"
+          type="audio"
+          controls={true}
+        />
       </div>
     </div>
   );
