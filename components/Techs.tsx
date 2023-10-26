c765d10d2048790dcf2439a21d02d219a4120a24
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-26 19:05:27 +0300

    Update Techs.tsx

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -9,13 +9,13 @@ const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
       <h2 className='font-bold text-lg uppercase'>Favori Şarkım - Enercii</h2>
       <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
 
-      <div className="react-player-wrapper">
-        <ReactPlayer
-          url="https://media.discordapp.net/attachments/1140520610141847552/1167127062516080776/Dilan_Polat_-_Enercii720P_HD.mp4"
-          type="audio"
-          controls={true}
-        />
-      </div>
+<div className="react-player-wrapper">
+  <ReactPlayer
+    url="https://media.discordapp.net/attachments/1140520610141847552/1167127062516080776/Dilan_Polat_-_Enercii720P_HD.mp4"
+    type="audio"
+    controls={true}
+  />
+</div>
     </div>
   );
 };
