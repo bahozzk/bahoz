b8ed8e9ee9436867c423b9640edaf40ebfa95666
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-26 18:23:42 +0300

    Update Techs.tsx

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -12,28 +12,17 @@ const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
       href: 'https://www.javascript.com/'
     }
   ];
+  
+  // Spotify embed kodu
+  const spotifyEmbedCode = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3a8mVeic6p5DdMSbsQXWIx?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
 
   return (
     <div className='my-20 md:px-8 w-full flex flex-col items-center'>
       <h2 className='font-bold text-lg uppercase'>Teknolojiler</h2>
       <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
-        {techs.map((data, index) => (
-          <a
-            target={'_blank'}
-            rel="noreferrer"
-            href={data.href}
-            className={`m-6 flex flex-row items-center`}
-            key={`techs${index}`}
-          >
-            {data.icon}
-          </a>
-        ))}
-
-        <ReactPlayer
-          url="https://cdn.discordapp.com/attachments/1140520610141847552/1167117259681046548/enerci.mp3"
-          type="audio"
-          controls={true}
-        />
+        {techs.map((data, index) => <a target={'_blank'} rel="noreferrer" href={data.href} className={`m-6 flex flex-row items-center`} key={`techs${index}`}>
+          {data.icon}
+        </a>)}
       </div>
     </div>
   );
