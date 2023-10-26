c90f0508dbea69908fa81e5b7d7f8b28ab433884
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-26 19:22:27 +0300

    Update Techs.tsx

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -10,7 +10,7 @@ const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
   <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
     <div className="react-player-wrapper">
       <ReactPlayer
-        url="https://media.discordapp.net/attachments/1140520610141847552/1167127062516080776/Dilan_Polat_-_Enercii720P_HD.mp4"
+        url="https://youtu.be/fQBqaga9ElU?si=54GQ26ZU5DC8xgPH"
         type="audio"
         controls={true}
       />
