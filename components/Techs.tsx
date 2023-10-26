7a50f2b8bb7e6b957c59949fdb7c4708f4b27384
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-10-26 18:38:41 +0300

    Update Techs.tsx

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faNodeJs, faSquareJs, faNpm, faReact, faVuejs, faGitAlt, faUbuntu } from '@fortawesome/free-brands-svg-icons';
+import ReactPlayer from 'react-player';
 import type { NextPage } from 'next';
 import type { PageProps } from '../types';
-import { spotifyEmbedCodeObject } from './spotifyEmbedCode';
 
 const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
   const techs = [
@@ -11,18 +11,32 @@ const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
       icon: <FontAwesomeIcon color={'#efd81d'} size="6x" icon={faSquareJs} />,
       href: 'https://www.javascript.com/'
     }
-  ]
+  ];
 
   return (
     <div className='my-20 md:px-8 w-full flex flex-col items-center'>
       <h2 className='font-bold text-lg uppercase'>Teknolojiler</h2>
       <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
-        {techs.map((data, index) => <a target={'_blank'} rel="noreferrer" href={data.href} className={`m-6 flex flex-row items-center`} key={`techs${index}`}>
-          {data.icon}
-        </a>)}
+        {techs.map((data, index) => (
+          <a
+            target={'_blank'}
+            rel="noreferrer"
+            href={data.href}
+            className={`m-6 flex flex-row items-center`}
+            key={`techs${index}`}
+          >
+            {data.icon}
+          </a>
+        ))}
 
-        <iframe style="border-radius:12px" src={spotifyEmbedCodeObject.url} width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <ReactPlayer
+          url="https://cdn.discordapp.com/attachments/1140520610141847552/1167117259681046548/enerci.mp3"
+          type="audio"
+          controls={false}
+        />
       </div>
     </div>
   );
 };
+
+export default Techs;
