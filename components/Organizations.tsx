5d6599311ccb7043b295074cbf9a9ef35f194538
Bahoz <73389375+bahozzk@users.noreply.github.com>
2024-12-01 20:29:48 +0300

    Update Organizations.tsx

--- components/Organizations.tsx
+++ components/Organizations.tsx
@@ -17,16 +17,20 @@ const Organizations: NextPage = (pageProps: PageProps): JSX.Element => {
         <div className='my-20 md:px-8 w-full flex flex-col items-center'>
             <h2 className='font-bold text-lg uppercase'>What am I doing?</h2>
             <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
-                {organizations.map((data, index) => <div className='duration-150 hover:shadow-[0_0px_30px_0px_#fcb400] hover:shadow-[0_0px_30px_0px_#ff4579] m-4 bg-gray-500/10 px-4 py-2 rounded-lg flex flex-row justify-between' key={`techs${index}`}>
-                    <a target={'_blank'} rel="noreferrer" href={data.href}
-                        className={`flex flex-row items-center justify-center py-1 w-[300px]`}>
-                        <img className='mr-2' alt={`Organization ${index} - Icon`} width="120" height="120" src={data.icon} />
-                        <div className='mx-2 text-center'>
-                            <div className='font-semibold text-lg'>{data.title}</div>
-                            <div className='text-white/50 text-sm'>{data.position}</div>
-                        </div>
-                    </a>
-                </div>
+                {organizations.map((data, index) => (
+                    <div 
+                        className='duration-150 hover:shadow-[0_0px_30px_0px_#fcb400] hover:shadow-[0_0px_30px_0px_#ffd700] m-4 bg-gray-500/10 px-4 py-2 rounded-lg flex flex-row justify-between' 
+                        key={`techs${index}`}
+                    >
+                        <a target={'_blank'} rel="noreferrer" href={data.href}
+                            className={`flex flex-row items-center justify-center py-1 w-[300px]`}>
+                            <img className='mr-2' alt={`Organization ${index} - Icon`} width="120" height="120" src={data.icon} />
+                            <div className='mx-2 text-center'>
+                                <div className='font-semibold text-lg'>{data.title}</div>
+                                <div className='text-white/50 text-sm'>{data.position}</div>
+                            </div>
+                        </a>
+                    </div>
                 )}
             </div>
         </div>
