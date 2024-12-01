1508f485003a6ae0d5671e3ab3e34a8df36b77bd
Bahoz <73389375+bahozzk@users.noreply.github.com>
2024-12-01 19:44:05 +0300

    Update Techs.tsx

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVolleyballBall } from '@fortawesome/free-solid-svg-icons'; // Voleybol ikonu
+import { faVolleyballBall } from '@fortawesome/free-solid-svg-icons';
 import type { NextPage } from 'next';
 import type { PageProps } from '../types';
 
@@ -7,34 +7,34 @@ const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
   const techs = [
     {
       icon: (
-        <img 
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/1280px-Flag_of_the_United_Kingdom_%283-5%29.svg.png" 
-          alt="UK Flag" 
-          className="w-full h-full object-cover rounded-lg" 
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/1280px-Flag_of_the_United_Kingdom_%283-5%29.svg.png"
+          alt="UK Flag"
+          className="w-16 h-16"
         />
       ),
-      href: '',
-      description: 'B1 English'
+      description: 'B1 English',
+      hoverColors: 'bg-gradient-to-r from-blue-500 via-red-500 to-white',
     },
     {
       icon: (
-        <img 
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1920px-Flag_of_France.svg.png" 
-          alt="France Flag" 
-          className="w-full h-full object-cover rounded-lg" 
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1920px-Flag_of_France.svg.png"
+          alt="France Flag"
+          className="w-16 h-16"
         />
       ),
-      href: '',
-      description: 'A1 French'
+      description: 'A1 French',
+      hoverColors: 'bg-gradient-to-r from-blue-500 via-white to-red-500',
     },
     {
       icon: (
-        <div className="w-full h-full flex items-center justify-center bg-yellow-100 rounded-lg">
+        <div className="w-16 h-16 flex items-center justify-center">
           <FontAwesomeIcon color="#ffcc00" size="3x" icon={faVolleyballBall} />
         </div>
       ),
-      href: 'https://grabagame.com/volleyball-skill-levels/',
-      description: 'B+ Volleyball'
+      description: 'B+ Volleyball',
+      hoverColors: 'bg-yellow-500',
     },
   ];
 
@@ -45,18 +45,18 @@ const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
       </h2>
       <div className="flex flex-row flex-wrap items-center justify-center gap-8 max-w-4xl">
         {techs.map((data, index) => (
-          <a
-            href={data.href || '#'}
-            target={data.href ? '_blank' : '_self'}
-            rel="noopener noreferrer"
-            className="group w-32 h-40 p-4 bg-gray-100 rounded-lg shadow-md flex flex-col items-center justify-between transition-transform transform hover:scale-105"
+          <div
             key={`techs${index}`}
+            className={`group relative m-4 w-40 h-48 p-4 bg-black rounded-lg shadow-lg overflow-hidden flex flex-col items-center justify-between transition-all duration-300 hover:${data.hoverColors}`}
           >
-            <div className="w-24 h-24 mb-3">{data.icon}</div>
-            <p className="text-center text-gray-600 group-hover:text-gray-800 font-medium">
+            <div
+              className={`absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${data.hoverColors} z-0`}
+            ></div>
+            <div className="z-10">{data.icon}</div>
+            <p className="z-10 text-center text-white mt-4 text-lg font-medium">
               {data.description}
             </p>
-          </a>
+          </div>
         ))}
       </div>
     </div>
