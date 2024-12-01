0e33f1993030b305f51ff5ba4324e1e68ede0a0a
Bahoz <73389375+bahozzk@users.noreply.github.com>
2024-12-01 20:43:34 +0300

    Update AboutMe.tsx

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faInstagram, faX, faDiscord, faYoutube } from '@fortawesome/free-brands-svg-icons'; // faTwitter yerine faX ekledik
+import { faGithub, faInstagram, faTwitter, faDiscord, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import type { NextPage } from 'next';
 import type { PageProps } from '../types';
 
@@ -14,7 +14,7 @@ const AboutMe: NextPage = (pageProps: PageProps): JSX.Element => {
             href: '/instagram'
         },
         {
-            icon: faX, // Twitter yerine X logosu kullanıldı
+            icon: faTwitter,
             href: '/x'
         },
         {
@@ -27,11 +27,13 @@ const AboutMe: NextPage = (pageProps: PageProps): JSX.Element => {
         }
     ];
 
-    const content = <p className='text-center my-4 text-lg text-white/80'>
-        Hiya! Im Bahoz,
-        <br></br>
-        I am interested in programming but I dont know any languages 🙃
-    </p>;
+    const content = (
+        <p className='text-center my-4 text-lg text-white/80'>
+            Hiya! I'm Bahoz,
+            <br />
+            I am interested in programming but I don't know any languages 🙃
+        </p>
+    );
 
     return (
         <div id="aboutme" className='my-4 md:px-8 w-full mb-20 flex flex-col items-center'>
@@ -40,9 +42,21 @@ const AboutMe: NextPage = (pageProps: PageProps): JSX.Element => {
                 <div className='block'>
                     {content}
                     <div className='flex flex-row flex-wrap items-center justify-center'>
-                        {links.map((data, index) => <a className='m-3' rel="noreferrer" target={'_blank'} href={data.href} key={`link${index}`}>
-                            <FontAwesomeIcon className='text-white/90 hover:text-orange-400/90 duration-150' size={'2x'} icon={data.icon} />
-                        </a>)}
+                        {links.map((data, index) => (
+                            <a 
+                                className='m-3' 
+                                rel="noreferrer noopener" 
+                                target="_blank" 
+                                href={data.href} 
+                                key={`link${index}`}
+                            >
+                                <FontAwesomeIcon 
+                                    className='text-white/90 hover:text-orange-400/90 duration-150' 
+                                    size={'2x'} 
+                                    icon={data.icon} 
+                                />
+                            </a>
+                        ))}
                     </div>
                 </div>
             </div>
