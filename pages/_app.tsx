b8d402531cc30b22aab9828e5f0e762abe4400a4
Bahoz <73389375+bahozzk@users.noreply.github.com>
2024-12-02 07:06:28 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import type { PageProps } from '../types';
 import Head from 'next/head';
+import Script from 'next/script'; // Next.js için script yönetimi
 
 config.autoAddCss = false;
 
@@ -27,6 +28,33 @@ function MyApp({ Component, pageProps }: AppProps<PageProps>): JSX.Element {
       <link rel="apple-touch-icon" href="https://mc-heads.net/head/BahozzK/400" sizes="512x512"></link>
       <title>BahozzK</title>
     </Head>
+
+    {/* Google Tag Manager */}
+    <Script
+      id="google-tag-manager"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KW2MFKH2');
+        `,
+      }}
+    />
+
+    {/* Google Tag Manager (noscript) */}
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-KW2MFKH2"
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      ></iframe>
+    </noscript>
+    {/* End Google Tag Manager */}
+
     <Component {...pageProps} />
     <Analytics />
   </>
