4e289e2dab3e6e3bb5b139fb7d1a3bfbd27d7000
Bahoz <73389375+bahozzk@users.noreply.github.com>
2025-09-21 12:31:46 +0200

    Update Techs.tsx

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -29,16 +29,6 @@ const Qualifications: NextPage = (pageProps: PageProps): JSX.Element => {
       hoverColors: 'from-blue-800 via-white to-red-500',  // Mavi tonunu daha koyu yaptım
       isUKFlag: false,
     },
-    {
-      icon: (
-        <div className="w-12 h-12 flex items-center justify-center"> {/* Voleybol topunun boyutunu büyüttüm */}
-          <FontAwesomeIcon color="#ffcc00" size="3x" icon={faVolleyballBall} />
-        </div>
-      ),
-      description: 'B+ Volleyball',
-      hoverColors: 'from-yellow-500 to-yellow-300',
-      isUKFlag: false,
-    },
   ];
 
   return (
